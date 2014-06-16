a9135635c73f82f10feaa4a0cebf7b97f6bb7ba8
Peter Marton <peter@secretsaucepartners.com>
2014-06-16 21:59:15 +0200

    Add PID controller.

--- README.md
+++ README.md
@@ -3,21 +3,26 @@ spark-core-pid-temperature
 
 Spark Core **PID** **temperature** controlling with **DS18B20**.
 
-!!! This repo isn't done yet. !!!
----------------------------------
+    //TODO: set point temperature via spark cloud
 
 What is this?
 -------------
 Controlling PWM output to reach the point temperature. 
 Doing this with a PID controller.
 
-![http://www.podmerchant.com/newsletter/68-temperature-overshoot.jpg][1]
+![http://www.podmerchant.com/newsletter/68-temperature-overshoot.jpg][3]
 
 Used libraries
 -------------
-- DS18B20 - https://github.com/krvarma/Dallas_DS18B20_SparkCore
-- PID - *// TODO*
 
+**PID**
+[https://github.com/br3ttb/Arduino-PID-Library][1]
+*(with a small change to make it work on a Spark Core)*
 
-  [1]: http://www.podmerchant.com/newsletter/68-temperature-overshoot.jpg
+**DS18B20**
+[https://github.com/krvarma/Dallas_DS18B20_SparkCore][2]
+
+  [1]: https://github.com/br3ttb/Arduino-PID-Library
+  [2]: https://github.com/krvarma/Dallas_DS18B20_SparkCore
+  [3]: http://www.podmerchant.com/newsletter/68-temperature-overshoot.jpg
   
--- brew.ino
+++ brew.ino
@@ -1,13 +1,36 @@
-#include "ds18b20/DS18B20.h"
-#include "ds18b20/OneWire.h"
+// https://github.com/br3ttb/Arduino-PID-Library
+#include "PID.h"
 
-DS18B20 ds18b20 = DS18B20(D2);
+// https://github.com/krvarma/Dallas_DS18B20_SparkCore
+#include "DS18B20.h"
+#include "OneWire.h"
+
+DS18B20 ds18b20 = DS18B20(D1);
+unsigned long lastTempRequest = 0;
 char szInfo[64];
+
+int RelayPin = A6;
+
+//Define Variables we'll be connecting to
+double Setpoint, Input, Output;
+
+//Specify the links and initial tuning parameters
+PID myPID(&Input, &Output, &Setpoint,2,5,1, DIRECT);
 
  
  // Setup
 void setup() {
     Serial1.begin(9600);
+    
+    Spark.variable("temperature", &szInfo, STRING);
+    
+    pinMode(RelayPin, OUTPUT);
+    Input = 0;
+    
+    Setpoint = 40;
+
+    //turn the PID on
+    myPID.SetMode(AUTOMATIC);
 }
  
  // Loop
@@ -18,14 +41,25 @@ void loop() {
         ds18b20.resetsearch();
         delay(250);
         
+        // Turn off
+        digitalWrite(RelayPin, LOW);
+        
         return;       
     }
     
     float celsius = ds18b20.getTemperature();
   
-    sprintf(szInfo, "{ temp: %2.2f, chip: \"%s\")", celsius, ds18b20.getChipName());
+    sprintf(szInfo, "%2.2f - %2.2f", celsius, (Output / 255) * 100);
+    
+    if (millis() - lastTempRequest >= 2000) {
+        Spark.publish("tmpinfo", szInfo);
+        lastTempRequest = millis();
+    }
+    
+    // PID
+    Input = (double) celsius;
   
-    Spark.publish("tmpinfo", szInfo);
+    myPID.Compute();
   
-    delay(1000);
+    analogWrite(RelayPin, Output);
 }
